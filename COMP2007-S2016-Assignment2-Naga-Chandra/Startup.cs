7afa91f5d0f3ba14adbc8f9b5de6c4668131a8e7
chandra <chandra@CHANDU>
2016-07-31 23:28:22 -0400

    Add project files.

---
+++ COMP2007-S2016-Assignment2-Naga-Chandra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_S2016_Assignment2_Naga_Chandra.Startup))]
+namespace COMP2007_S2016_Assignment2_Naga_Chandra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
